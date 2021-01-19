273ae2b719ada440f95e01127463528eb45f3e70
Christian Cueni <christian.cueni@iterativ.ch>
2021-01-19 20:17:09 +0100

    Add temperature check

--- lib/controller.ex
+++ lib/controller.ex
@@ -21,5 +21,15 @@ defmodule Controller do
           _ -> {:ok, %{ controller | current_step_index: controller.current_step_index - 1 }}
         end
     end
+
+    def check_temp(measured, %Controller{} = controller) do
+      target_temp = Enum.at(controller.profile.steps, controller.current_step_index).temperature
+      delta = measured - target_temp
+      cond do
+        delta > 0.5 -> :too_warm
+        delta < -0.5 -> :too_cold
+        true -> :ok
+      end
+    end
   
   end
